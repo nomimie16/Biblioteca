69c90ece0dc35b88640c5fc3b26a3743f7b6b893
LIGNIER Noemie <831noemie@gmail.com>
2026-03-25 10:30:53 +0100

    class Book + program

--- LibraryManager/LibraryManager.App/Program.cs
+++ LibraryManager/LibraryManager.App/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        // List<T> est une collection générique qui peut contenir plusieurs éléments du type T
+        List<Book> items = new List<Book>
+        {
+            new Book { Name = "Alice au pays des merveilles", Type = "Aventure" },
+            new Book { Name = "Hunger Games", Type = "Science fiction" },
+            new Book { Name = "Romeo et Juliette", Type = "Romance" },
+
+        };
+
+        Console.WriteLine("Liste des livres : ");
+        foreach (var item in items)
+        {
+            Console.WriteLine(item.Name +" : "+ item.Type);
+        }
+    }
+}
