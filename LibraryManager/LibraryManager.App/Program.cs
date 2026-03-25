6ff2e6769aa81cde5d6a08b17c9e04883603d24f
LIGNIER Noemie <831noemie@gmail.com>
2026-03-25 10:39:30 +0100

    Etape 3 : Filtre des livres de type aventure

--- LibraryManager/LibraryManager.App/Program.cs
+++ LibraryManager/LibraryManager.App/Program.cs
@@ -2,8 +2,8 @@
 {
     static void Main(string[] args)
     {
-        // List<T> est une collection générique qui peut contenir plusieurs éléments du type T
-        List<Book> items = new List<Book>
+        // Creation de liste de livres
+        List<Book> books = new List<Book>
         {
             new Book { Name = "Alice au pays des merveilles", Type = "Aventure" },
             new Book { Name = "Hunger Games", Type = "Science fiction" },
@@ -11,10 +11,21 @@
 
         };
 
+        // Boucle pour afficher les livres et leur type
         Console.WriteLine("Liste des livres : ");
-        foreach (var item in items)
+        foreach (var book in books)
         {
-            Console.WriteLine(item.Name +" : "+ item.Type);
+            Console.WriteLine(book.Name +" : "+ book.Type);
         }
+
+        // Filtre les livres de type aventure et affiche
+        var categoryAbooks = books.Where(book => book.Type == "Aventure");
+        foreach (var book in categoryAbooks)
+        {
+            Console.WriteLine($"Livre de type aventure : {book.Name}");
+        }
+
+       
+
     }
 }
